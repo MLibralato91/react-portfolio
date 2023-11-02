50081592fefb93f400e1b18f5ed0e3927866db22
MLibralato91 <michelelibralato911@gmail.com>
2023-11-02 18:50:46 +0100

    add loader

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,6 +10,7 @@ import {
   faPhp,
   faVuejs,
 } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -24,56 +25,59 @@ const About = () => {
   })
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos,
-          non minus tenetur impedit perspiciatis reprehenderit? Alias incidunt
-          dolor eveniet, consequatur dolore accusamus quos. Quos?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos,
-          non minus tenetur impedit perspiciatis reprehenderit? Alias incidunt
-          dolor eveniet, consequatur dolore accusamus quos. Quos?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos,
-          non minus tenetur impedit perspiciatis reprehenderit? Alias incidunt
-          dolor eveniet, consequatur dolore accusamus quos. Quos?
-        </p>
-      </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos,
+            non minus tenetur impedit perspiciatis reprehenderit? Alias incidunt
+            dolor eveniet, consequatur dolore accusamus quos. Quos?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos,
+            non minus tenetur impedit perspiciatis reprehenderit? Alias incidunt
+            dolor eveniet, consequatur dolore accusamus quos. Quos?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quos,
+            non minus tenetur impedit perspiciatis reprehenderit? Alias incidunt
+            dolor eveniet, consequatur dolore accusamus quos. Quos?
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faVuejs} color="#41b883" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faPhp} color="#6181b6" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faLaravel} color="#ef533f" />
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faVuejs} color="#41b883" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faPhp} color="#6181b6" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faLaravel} color="#ef533f" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
