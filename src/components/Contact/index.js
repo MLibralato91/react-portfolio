f754e53a50b549519fab9a001a80b2b910b05ba3
MLibralato91 <michelelibralato911@gmail.com>
2023-11-03 17:35:08 +0100

    improved the map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,6 @@
 import './index.scss'
 import { useEffect, useRef, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import AnimatedLetters from '../AnimatedLetters'
 import Loader from 'react-loaders'
 import emailjs from '@emailjs/browser'
@@ -87,6 +88,26 @@ const Contact = () => {
               </ul>
             </form>
           </div>
+        </div>
+        <div className="info-map">
+          Michele Libralato
+          <br />
+          Italia,
+          <br />
+          Latina LT, 04100 <br />
+          <span>michele.libralato.ml@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer
+            center={[44.96366, 19.61045]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, come over for a cup of coffee :</Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
