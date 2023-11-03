b935274670c7f5b8c4709df7a0f83ddb4d036e57
MLibralato91 <michelelibralato911@gmail.com>
2023-11-03 11:03:12 +0100

    add new logo M

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import LogoTitle from '../../assets/images/logo-s.png'
+import LogoTitle from '../../assets/images/logo-m.png'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,6 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoM from '../../assets/images/logo-m.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
@@ -9,8 +8,7 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
-      <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      <img src={LogoM} alt="logo" />
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
